7e4d704a07b821ec15aa2152b5f40ad10c212b6a
JaRRa <agsilvente2002@gmail.com>
2023-02-21 17:49:16 +0100

    Ejercicio 4 hecho

--- src/ejercicio4/Clase4.java
+++ src/ejercicio4/Clase4.java
@@ -2,26 +2,33 @@ package ejercicio4;
 
 import java.util.Arrays;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Clase4 {
-    public static void main(String[] args) {
-        System.out.println("Nueva");
-        System.out.println("Vieja");
 
-
-        Scanner sc = new Scanner(System.in);
-        Random rd = new Random();
-        int numeroPosArray;
-        System.out.println("Introduce el numero de posiciones del array");
-        numeroPosArray = sc.nextInt();
-
-        //int[] mostrarNumeros = new int[10];
-        int[] numeros = new int[numeroPosArray];
-        for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = rd.nextInt(301);
+    private static int[] array;
+    public static Boolean rellenarArray(int numero){
+        if(numero > 0 && numero <= 10){
+            Random rd = new Random();
+            System.out.println("Introduce el numero de posiciones del array");
+            array = new int [numero];
+            for(int i = 0; i < array.length; i++){
+                array[i] = rd.nextInt(301);
+            }
+            return true;
         }
-        System.out.println(Arrays.toString(numeros));
-
+        return false;
+    }
+    public static String mostrarArrayDeDigitos(int ultimoDigito){
+        int pos = 0;
+        int [] nuevoArray = new int[10];
+        for(int i = 0; i < array.length; i++){
+            if(array[i] % 10 == ultimoDigito){
+                for(int e = 0; e < nuevoArray.length; e++){
+                    nuevoArray[pos] = array[i];
+                }
+                pos++;
+            }
+        }
+        return Arrays.toString(nuevoArray);
     }
 }
---
+++ src/ejercicio4/Main.java
@@ -0,0 +1,11 @@
+package ejercicio4;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Clase4.rellenarArray(9));
+        System.out.println(Clase4.mostrarArrayDeDigitos(5));
+
+    }
+}
