58ad07d99ca52ddab2e747760acb7916a956db4c
JaRRa <agsilvente2002@gmail.com>
2023-02-21 18:52:25 +0100

    Ejercicio 6 hecho

---
+++ src/ejercicio6/Ejercicio6.java
@@ -0,0 +1,30 @@
+package ejercicio6;
+
+import java.util.Arrays;
+
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        int pos = 4;
+        int posInversa = 0;
+        int [] array = {1, 2 ,3, 4, 5};
+        int[] arrayInverso = new int[5];
+
+        for(int i = array.length - 1; i >= 0; i--){
+            if(i == pos){
+                for(int e = 0; e < arrayInverso.length; e++){
+                    if(posInversa == e){
+                        arrayInverso[posInversa] = array[i];
+                        break;
+                    }
+
+                }
+                posInversa++;
+                pos--;
+            }
+
+        }
+        System.out.println(Arrays.toString(array));
+        System.out.println(Arrays.toString(arrayInverso));
+
+    }
+}
