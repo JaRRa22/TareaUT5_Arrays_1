d7b0b0442284cde4eac2321d8d0824c2480dc745
JaRRa <agsilvente2002@gmail.com>
2023-02-21 10:23:36 +0100

    Ejercicio 4 empezado

--- src/ejercicio4/Clase4.java
+++ src/ejercicio4/Clase4.java
@@ -1,7 +1,23 @@
 package ejercicio4;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Clase4 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+        int numeroPosArray;
+        System.out.println("Introduce el numero de posiciones del array");
+        numeroPosArray = sc.nextInt();
+
+        //int[] mostrarNumeros = new int[10];
+        int[] numeros = new int[numeroPosArray];
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = rd.nextInt(301);
+        }
+        System.out.println(Arrays.toString(numeros));
 
     }
 }
